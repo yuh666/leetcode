b67779670c1c3862f48b0d8b8cd3be7e535f5658
yuhao <yuhao@daojia-inc.com>
2019-08-30 15:16:47 +0800

    two sum

---
+++ liangshuzhihe_1/Solution.java
@@ -0,0 +1,40 @@
+package liangshuzhihe_1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * <p>
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ * <p>
+ * 示例:
+ * <p>
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ * <p>
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/two-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ **/
+public class Solution {
+
+    public int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            Integer index = map.get(target - num);
+            if (index != null && index != i) {
+                return new int[]{index, i};
+            } else {
+                map.put(num, i);
+            }
+        }
+        return null;
+    }
+}
