125fe5930e5bd137dbfb71b5b19225c4d475933a
yuhao <yuhao@daojia-inc.com>
2019-08-31 11:15:09 +0800

    reverseKGroup

---
+++ fanzhuanlianbiao_206/Solution.java
@@ -0,0 +1,45 @@
+package fanzhuanlianbiao_206;
+
+/**
+ * 反转一个单链表。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ * 进阶:
+ * 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+ * <p>
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/reverse-linked-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+// Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+public class Solution {
+
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode newHead = null;
+        while (head != null){
+            ListNode next = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = next;
+        }
+        return newHead;
+    }
+
+}
---
+++ liangliangjiaohuanlianbiaozhongdejiedian_24/Solution.java
@@ -0,0 +1,44 @@
+package liangliangjiaohuanlianbiaozhongdejiedian_24;
+
+/**
+ * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+ * <p>
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ * <p>
+ *  
+ * <p>
+ * 示例:
+ * <p>
+ * 给定 1->2->3->4, 你应该返回 2->1->4->3.
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/swap-nodes-in-pairs
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+// Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+public class Solution {
+
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode curr = dummy;
+        while (curr.next != null && curr.next.next != null) {
+            ListNode node1 = curr.next;
+            ListNode node2 = node1.next;
+            curr.next = node2;
+            node1.next = node2.next;
+            node2.next = node1;
+            curr = node1;
+        }
+        return dummy.next;
+    }
+}
