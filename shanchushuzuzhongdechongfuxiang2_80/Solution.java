aeb84bf2cef52a69405264973689716074e892a8
yuhao <yuhao@daojia-inc.com>
2019-08-31 19:00:23 +0800

    删除重复元素

---
+++ shanchushuzuzhongdechongfuxiang2_80/Solution.java
@@ -0,0 +1,39 @@
+package shanchushuzuzhongdechongfuxiang2_80;
+
+import java.util.Arrays;
+
+/**
+ * 给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素最多出现两次，返回移除后数组的新长度。
+ * <p>
+ * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 给定 nums = [1,1,1,2,2,3],
+ * <p>
+ * 函数应返回新长度 length = 5, 并且原数组的前五个元素被修改为 1, 1, 2, 2, 3 。
+ * <p>
+ * 你不需要考虑数组中超出新长度后面的元素。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution {
+
+    public int removeDuplicates(int[] nums) {
+        if (nums.length < 3) {
+            return nums.length;
+        }
+        int index = 2;
+        for (int i = 2; i < nums.length; i++) {
+            if(nums[i] != nums[index-2]){
+                nums[index++] = nums[i];
+            }
+        }
+        return index;
+    }
+
+    public static void main(String[] args) {
+    }
+}
