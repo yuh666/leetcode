19eb1f3511e5c4ca501f2558b59b5d52b6e638ac
yuhao <yuhao@daojia-inc.com>
2019-08-31 09:15:06 +0800

    删除倒数第N个节点

---
+++ shanchulianbiaodedaoshudingejiedian_19/Solution.java
@@ -0,0 +1,60 @@
+package shanchulianbiaodedaoshudingejiedian_19;
+
+/**
+ * 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+ * <p>
+ * 示例：
+ * <p>
+ * 给定一个链表: 1->2->3->4->5, 和 n = 2.
+ * <p>
+ * 当删除了倒数第二个节点后，链表变为 1->2->3->5.
+ * 说明：
+ * <p>
+ * 给定的 n 保证是有效的。
+ * <p>
+ * 进阶：
+ * <p>
+ * 你能尝试使用一趟扫描实现吗？
+ * <p>
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+
+// Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+public class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null) {
+            return null;
+        }
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode curr = dummy;
+        for (int i = 0; i < n && curr != null; i++) {
+            curr = curr.next;
+        }
+        if (curr == null) {
+            return head;
+        }
+        ListNode pre = dummy;
+        while (curr.next != null) {
+            pre = pre.next;
+            curr = curr.next;
+        }
+        pre.next = pre.next.next;
+        return dummy.next;
+    }
+
+}
