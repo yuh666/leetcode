c25dbbe5d9172e93a315916d4f55560173bde63c
yuhao <yuhao@daojia-inc.com>
2019-09-03 13:19:34 +0800

    house robber

---
+++ huadongchuangkouzuidazhi_239/Solution.java
@@ -0,0 +1,72 @@
+package huadongchuangkouzuidazhi_239;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+
+/**
+ * 给定一个数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k 个数字。滑动窗口每次只向右移动一位。
+ * <p>
+ * 返回滑动窗口中的最大值。
+ * <p>
+ *  
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: nums = [1,3,-1,-3,5,3,6,7], 和 k = 3
+ * 输出: [3,3,5,5,6,7]
+ * 解释:
+ * <p>
+ * 滑动窗口的位置                最大值
+ * ---------------               -----
+ * [1  3  -1] -3  5  3  6  7       3
+ * 1 [3  -1  -3] 5  3  6  7       3
+ * 1  3 [-1  -3  5] 3  6  7       5
+ * 1  3  -1 [-3  5  3] 6  7       5
+ * 1  3  -1  -3 [5  3  6] 7       6
+ * 1  3  -1  -3  5 [3  6  7]      7
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 你可以假设 k 总是有效的，在输入数组不为空的情况下，1 ≤ k ≤ 输入数组的大小。
+ * <p>
+ *  
+ * <p>
+ * 进阶：
+ * <p>
+ * 你能在线性时间复杂度内解决此题吗？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/sliding-window-maximum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution {
+
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        LinkedList<Integer> list = new LinkedList<>();
+        ArrayList<Integer> resList = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (list.size() > 0 && i - list.getFirst() == k) {
+                list.removeFirst();
+            }
+            while (list.size() > 0 && nums[list.getLast()] < nums[i]) {
+                list.removeLast();
+            }
+            list.addLast(i);
+            if (i >= k - 1) {
+                resList.add(nums[list.getFirst()]);
+            }
+        }
+        int[] res = new int[resList.size()];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = resList.get(i);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] window = new Solution().maxSlidingWindow(new int[]{1, 3, 1, 2, 0, 5}, 3);
+        System.out.println(Arrays.toString(window));
+    }
+}
